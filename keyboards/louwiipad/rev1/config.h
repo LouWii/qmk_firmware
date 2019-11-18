56d055ccd978606a582f73e4c93d621632f84d42
Louwii <link14.online@gmail.com>
2019-11-17 20:07:53 -0800

    Fix diode direction, fix bootloader type

--- keyboards/louwiipad/rev1/config.h
+++ keyboards/louwiipad/rev1/config.h
@@ -47,7 +47,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
-#define DIODE_DIRECTION ROW2COL
+#define DIODE_DIRECTION COL2ROW
 
 // Rotary encoders
 #define ENCODERS_PAD_A { D6, F6, D2, F1, B7, E6 }
