bb959d05652ecea4ad4f606f9417441dfdb6009d
Louwii <link14.online@gmail.com>
2019-11-18 22:42:09 -0800

    enable rgb, add rgb control via encoders, add bkl layer for RGB actions

--- keyboards/louwiipad/keymaps/default/keymap.c
+++ keyboards/louwiipad/keymaps/default/keymap.c
@@ -19,8 +19,9 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BL 0
-#define _UP 1
+#define _BL  0
+#define _UP  1
+#define _BKL 2
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
@@ -38,7 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_UP] = LAYOUT(
     KC_1, KC_2, KC_3, KC_4,
     KC_A, KC_B, KC_C, KC_D
-  )
+  ),
+  [_BKL] = LAYOUT(
+    RGB_TOG,        RGB_MOD,  KC_NO, KC_NO,
+    RGB_MODE_PLAIN, RGB_RMOD, KC_NO, KC_NO
+  ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -62,6 +67,95 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+
+  if (index == 0) { /* First encoder */
+    if (clockwise) {
+      // tap_code(KC_PGDN);
+      // tap_code(RGB_VAI);
+      rgblight_increase_val();
+    } else {
+      // tap_code(KC_PGUP);
+      // tap_code(RGB_VAD);
+      rgblight_decrease_val();
+    }
+  } else if (index == 1) { /* Second encoder */  
+    if (clockwise) {
+      // tap_code(KC_PGDN);
+      // tap_code(RGB_VAI);
+      rgblight_increase_hue();
+    } else {
+      // tap_code(KC_PGUP);
+      // tap_code(RGB_VAD);
+      rgblight_decrease_hue();
+    }
+  } else if (index == 2) { /* Third encoder */
+    if (clockwise) {
+      // tap_code(KC_PGDN);
+      // tap_code(RGB_VAI);
+      rgblight_increase_sat();
+    } else {
+      // tap_code(KC_PGUP);
+      // tap_code(RGB_VAD);
+      rgblight_decrease_sat();
+    }
+  } else if (index == 3) { /* Fourth encoder */
+    if (clockwise) {
+      // tap_code(KC_PGDN);
+      // tap_code(RGB_VAI);
+      rgblight_increase_val();
+    } else {
+      // tap_code(KC_PGUP);
+      // tap_code(RGB_VAD);
+      rgblight_decrease_val();
+    }
+  } else if (index == 4) { /* Fifth encoder */
+    if (clockwise) {
+      // tap_code(KC_PGDN);
+      // tap_code(RGB_VAI);
+      rgblight_increase_val();
+    } else {
+      // tap_code(KC_PGUP);
+      // tap_code(RGB_VAD);
+      rgblight_decrease_val();
+    }
+  } else if (index == 5) { /* Sixth encoder */
+    if (clockwise) {
+      // tap_code(KC_PGDN);
+      // tap_code(RGB_VAI);
+      rgblight_step();
+    } else {
+      // tap_code(KC_PGUP);
+      // tap_code(RGB_VAD);
+      rgblight_step_reverse();
+    }
+  }
+}
+
+#ifdef OLED_DRIVER_ENABLE
+void oled_task_user(void) {
+  // Host Keyboard Layer Status
+  oled_write_P(PSTR("Layer: "), false);
+  switch (get_highest_layer(layer_state)) {
+    case _BL:
+      oled_write_P(PSTR("Default\n"), false);
+      break;
+    case _UP:
+      oled_write_P(PSTR("FN\n"), false);
+      break;
+    default:
+      // Or use the write_ln shortcut over adding '\n' to the end of your string
+      oled_write_ln_P(PSTR("Undefined"), false);
+  }
+
+  // Host Keyboard LED Status
+  // uint8_t led_usb_state = host_keyboard_leds();
+  // oled_write_P(led_usb_state & (1<<USB_LED_NUM_LOCK) ? PSTR("NUMLCK ") : PSTR("       "), false);
+  // oled_write_P(led_usb_state & (1<<USB_LED_CAPS_LOCK) ? PSTR("CAPLCK ") : PSTR("       "), false);
+  // oled_write_P(led_usb_state & (1<<USB_LED_SCROLL_LOCK) ? PSTR("SCRLCK ") : PSTR("       "), false);
+}
+#endif
 
 void matrix_init_user(void) {
 
--- keyboards/louwiipad/rev1/config.h
+++ keyboards/louwiipad/rev1/config.h
@@ -50,35 +50,35 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 
 // Rotary encoders
-#define ENCODERS_PAD_A { D6, F6, D2, F1, B7, E6 }
-#define ENCODERS_PAD_B { D4, F7, D3, F4, B0, F0 }
+#define ENCODERS_PAD_A { D4, F7, D3, F4, B0, F0 }
+#define ENCODERS_PAD_B { D6, F6, D2, F1, B7, E6 }
 //#define ENCODER_RESOLUTION 4
 
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
 // #define BACKLIGHT_LEVELS 3
 
-// #define RGB_DI_PIN E2
-// #ifdef RGB_DI_PIN
-//   #define RGBLED_NUM 16
-//   #define RGBLIGHT_HUE_STEP 8
-//   #define RGBLIGHT_SAT_STEP 8
-//   #define RGBLIGHT_VAL_STEP 8
-//   #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+#define RGB_DI_PIN B5
+#ifdef RGB_DI_PIN
+  #define RGBLED_NUM 4
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
 //   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 // /*== all animations enable ==*/
 //   #define RGBLIGHT_ANIMATIONS
 // /*== or choose animations ==*/
-//   #define RGBLIGHT_EFFECT_BREATHING
-//   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+  #define RGBLIGHT_EFFECT_BREATHING
+  #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+  #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 //   #define RGBLIGHT_EFFECT_SNAKE
 //   #define RGBLIGHT_EFFECT_KNIGHT
 //   #define RGBLIGHT_EFFECT_CHRISTMAS
 //   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //   #define RGBLIGHT_EFFECT_RGB_TEST
 //   #define RGBLIGHT_EFFECT_ALTERNATING
-// #endif
+#endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
