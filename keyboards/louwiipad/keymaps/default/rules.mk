5f8b76c927da22dd51e73e9b2df01ae8481c08eb
LouWii <link14.online@gmail.com>
2019-11-22 23:56:05 -0800

    looks like I did mess up the rules.mk - fixing them

--- keyboards/louwiipad/keymaps/default/rules.mk
+++ keyboards/louwiipad/keymaps/default/rules.mk
@@ -1,8 +1,3 @@
-# Enable keyboard RGB underglow
-RGBLIGHT_ENABLE = yes
-
-# Oled screen driver (+3904)
-OLED_DRIVER_ENABLE = yes
 
 # MIDI support (+2400 to 4200, depending on config)
 MIDI_ENABLE = yes
---
+++ keyboards/louwiipad/rev1/rules.mk
@@ -0,0 +1,6 @@
+
+# Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes
+
+# Oled screen driver (+3904)
+OLED_DRIVER_ENABLE = yes
